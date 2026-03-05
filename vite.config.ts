a4699e88b7d67d4dc3e71a6d77b940aeb83f5435
Lovesh Kamlesh Dabi <loveshsolutions@gmail.com>
2026-03-05 17:34:47 +0530

    Created vite.config.ts with Vite and React configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+// Vite and React configuration
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
 });
